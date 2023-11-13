c9716faa60b45b4afadfe94d5fa4533bc3480ba1
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2023-11-13 18:48:28 +0530

    fixed max_elements

--- Max_Element_In_Array.py
+++ Max_Element_In_Array.py
@@ -1,14 +1,14 @@
 def find_max(array):
     if not array:
-        return None  # or raise an exception, depending on your requirements
+        return None # this is to make sure that the array is not empty
     
     max_element = array[0]
     for element in array[1:]:
         if element > max_element:
-            max_element = element
+            max_element = element # will switch the element that is bigger than the max_element to the max_element
 
     return max_element
 
-array = [12, 45, 34, 6, 2]
+array = [12, 45, 34, 6, 2] # example of an array i used
 result = find_max(array)
 print(f"The maximum element in the array is {result}")
