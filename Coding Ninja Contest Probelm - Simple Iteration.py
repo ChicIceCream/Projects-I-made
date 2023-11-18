9cfb9dac23d054cc7e787cb1ab1b0c766cd22a9c
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2023-11-18 19:56:00 +0530

    Coding Ninja Contest problem

---
+++ Coding Ninja Contest Probelm - Simple Iteration.py
@@ -0,0 +1,15 @@
+'''
+You are given three integers, 'X', 'Y', and 'Z'.
+Your task is to discover and return all integers from the range of '1° to 'Z'. The integers should be such that when divided by 'X', they yield a remainder of Y°.
+Note The sequence of these returned integers can be in any order
+'''
+def iterateTillZ(list, x, y, z):
+    for index in range(len(list[0:z])):
+        if list[index] % x == y:
+            print(list[index])
+
+list = [1,2,3,4,5,6,7,8,9,10]
+x = int(input())
+y = int(input())
+z = int(input())
+iterateTillZ(list, x, y, z)
