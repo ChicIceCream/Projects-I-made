1f64868725a89463e739ec6b74a0916f255712bb
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2023-11-20 06:09:49 +0530

    Improved binary search

--- Binary Search for Coding Ninja.py
+++ Binary Search for Coding Ninja.py
@@ -1,30 +1,25 @@
 # Array submitted on Coding Ninja website
 # This binary search takes in input from the user on the array and target
 
-def binary_search(list, target):
+def binary_search(arr, target):
     
-    upperbound, lowerbound, found = len(list) - 1, 0, False
+    upperbound, lowerbound = len(arr) - 1, 0
     
     while lowerbound <= upperbound:
         index = (upperbound + lowerbound) // 2
 
-        if target == list[index]:
-            found = True
-            break
-        elif target > list[index]:
+        if target == arr[index]:
+            return index
+        elif target > arr[index]:
             lowerbound = index + 1
         else:
             upperbound = index - 1
 
-    if found:
-        print(index)
-    else:
-        print(-1)
-
+    return -1
 #Input
 N = int(input())
 list = list(map(int, input().split()))
 target = int(input())
 
 #Output
-binary_search(list, target)
+print(binary_search(list, target))
